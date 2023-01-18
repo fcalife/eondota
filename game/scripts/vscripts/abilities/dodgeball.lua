b21ab6697d90febe8254713a1cf8ce04732ec46a
Firetoad <fcalife@gmail.com>
2023-01-18 01:57:03 -0300

    dodgeball with rebounding

--- game/scripts/vscripts/abilities/dodgeball.lua
+++ game/scripts/vscripts/abilities/dodgeball.lua
@@ -5,18 +5,25 @@ function ability_dodgeball_throw:OnSpellStart()
 	local target = self:GetCursorPosition()
 
 	local speed = self:GetSpecialValueFor("speed")
+
+	self:ThrowBall(caster, target, speed)
+
+	caster:EmitSound("Hero_VengefulSpirit.MagicMissile")
+end
+
+function ability_dodgeball_throw:ThrowBall(thrower, target, speed)
 	local radius = self:GetSpecialValueFor("radius")
 
-	local direction = (target - caster:GetAbsOrigin()):Normalized()
+	local direction = (target - thrower:GetAbsOrigin()):Normalized()
 
 	local projectile = {
 		Ability				= self,
 		EffectName			= "particles/dodgeball/basic_throw.vpcf",
-		vSpawnOrigin		= caster:GetAttachmentOrigin(caster:ScriptLookupAttachment("attach_attack1")),
+		vSpawnOrigin		= thrower:GetAttachmentOrigin(thrower:ScriptLookupAttachment("attach_attack1")),
 		fDistance			= 3000,
 		fStartRadius		= radius,
 		fEndRadius			= radius,
-		Source				= caster,
+		Source				= thrower,
 		bHasFrontalCone		= false,
 		bReplaceExisting	= false,
 		iUnitTargetTeam		= DOTA_UNIT_TARGET_TEAM_ENEMY,
@@ -27,22 +34,57 @@ function ability_dodgeball_throw:OnSpellStart()
 		vVelocity			= Vector(direction.x, direction.y, 0) * speed,
 		bProvidesVision		= true,
 		iVisionRadius 		= 350,
-		iVisionTeamNumber 	= caster:GetTeam(),
+		iVisionTeamNumber 	= thrower:GetTeam(),
+		ExtraData			= {current_speed = speed}
 	}
 
 	ProjectileManager:CreateLinearProjectile(projectile)
-
-	caster:EmitSound("Hero_VengefulSpirit.MagicMissile")
 end
 
-function ability_dodgeball_throw:OnProjectileHit(target, location)
+function ability_dodgeball_throw:OnProjectileHit_ExtraData(target, location, data)
 	if target then
-		ApplyDamage({attacker = self:GetCaster(), victim = target, damage = 1000, damage_type = DAMAGE_TYPE_PURE})
+		if target:HasModifier("modifier_dodgeball_throwback") then
+			self:ThrowBall(target, self:GetCaster():GetAbsOrigin(), data.current_speed + 150)
 
-		target:EmitSound("Hero_VengefulSpirit.MagicMissileImpact")
+			target:EmitSound("Item.LotusOrb.Activate")
+		else
+			ApplyDamage({attacker = self:GetCaster(), victim = target, damage = 1000, damage_type = DAMAGE_TYPE_PURE})
+
+			target:EmitSound("Hero_VengefulSpirit.MagicMissileImpact")
+		end
 
 		return true
 	end
+end
+
+
+
+LinkLuaModifier("modifier_dodgeball_throwback", "abilities/dodgeball", LUA_MODIFIER_MOTION_NONE)
+
+ability_dodgeball_throwback = class({})
+
+function ability_dodgeball_throwback:OnSpellStart()
+	local caster = self:GetCaster()
+
+	caster:AddNewModifier(caster, self, "modifier_dodgeball_throwback", {duration = self:GetSpecialValueFor("duration")})
+
+	caster:EmitSound("DOTA_Item.LinkensSphere.Activate")
+end
+
+
+
+modifier_dodgeball_throwback = class({})
+
+function modifier_dodgeball_throwback:IsHidden() return false end
+function modifier_dodgeball_throwback:IsDebuff() return false end
+function modifier_dodgeball_throwback:IsPurgable() return false end
+
+function modifier_dodgeball_throwback:GetEffectName()
+	return "particles/items3_fx/lotus_orb_shield.vpcf"
+end
+
+function modifier_dodgeball_throwback:GetEffectAttachType()
+	return PATTACH_ABSORIGIN_FOLLOW
 end
 
 
